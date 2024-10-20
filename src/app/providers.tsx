361c8157a1c0301fcd709508d9b07fe2e2c799ac
Swapnadeep Mohapatra <41564532+swapnadeepmohapatra@users.noreply.github.com>
2024-10-20 11:53:03 +0530

    [Fixed] Lint Warnings

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,13 +1,15 @@
 "use client";
 
 import { DataProvider } from "@/contexts/DataContext";
-import { ReactNode } from "react";
+import { ReactNode, Suspense } from "react";
 import { SessionProvider } from "next-auth/react";
 
 export function Providers({ children }: { children: ReactNode }) {
   return (
-    <SessionProvider>
-      <DataProvider>{children}</DataProvider>
-    </SessionProvider>
+    <Suspense fallback={<div>Loading...</div>}>
+      <SessionProvider>
+        <DataProvider>{children}</DataProvider>
+      </SessionProvider>
+    </Suspense>
   );
 }
